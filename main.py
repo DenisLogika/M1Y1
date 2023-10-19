c87737ade0a03146f96ffe1a1cbb8b0112f00d32
e eye <142676437+DenisLogika@users.noreply.github.com>
2023-10-19 21:07:48 +0300

    main.py

--- main.py
+++ main.py
@@ -2,20 +2,32 @@ import typing
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from password import Ui_MainWindow
+import random
 
 class Widget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.password = Ui_MainWindow()
-        self.password.setupUi(self)
-
-def generation():
-    digital = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-btn_OK.connect.clicked(generation)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.btn_generate.clicked.connect(self.generate)
+    
+    def generate(self):
+        s_number = '0123456789'
+        s_alphabet = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+        pas = ''
+        if self.ui.cb_numbers.isChecked():
+            for i in range(30):
+                pas = pas + random.choice(s_number)
+        if self.ui.cb_alphabet.isChecked():
+            for i in range(30):
+                pas = pas + random.choice(s_alphabet)
+        if self.ui.cb_numbers.isChecked() and self.ui.cb_alphabet.isChecked():
+            for i in range(30):
+                pas = pas + random.choice(s_number + s_alphabet)
+        self.ui.result.setText(pas)
 
 app = QApplication([])
 ex = Widget()
+
 ex.show()
 app.exec_()
